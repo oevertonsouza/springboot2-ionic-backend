0709069a8a91201655fd1a02dabca9186843c475
Everton <oevertonsouza@gmail.com>
2020-01-19 21:39:54 -0300

    Tratamento de exceção

--- src/main/java/com/everton/cursomc/services/CategoriaServices.java
+++ src/main/java/com/everton/cursomc/services/CategoriaServices.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.everton.cursomc.domain.Categoria;
 import com.everton.cursomc.repositories.CategoriaRepository;
+import com.everton.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
@@ -17,7 +18,8 @@ public class CategoriaServices {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encotrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	
